2a9f1fd4159256cfa1586b08745701b9d68b4d06
Mickael972 <madallstar@gmail.com>
2025-04-18 15:04:34 +0200

    ajout de code python

---
+++ main.py
@@ -0,0 +1,19 @@
+age = 40
+print(age)
+
+string = "Hello, World!"
+print(string)
+
+5+5-5*5/5
+print(5+5-5*5/5)
+
+class voiture:
+    def __init__(self, marque, modele):
+        self.marque = marque
+        self.modele = modele
+
+ma_voiture = voiture ("Toyota", "Corolla")
+print(ma_voiture.marque, ma_voiture.modele)
+
+    
+        
